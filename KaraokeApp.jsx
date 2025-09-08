06327e935d3ba51541745736888e518e393d9e08
tranongtan-max <tranongtan@gmail.com>
2025-09-08 15:09:51 +0700

    Update KaraokeApp.jsx

--- KaraokeApp.jsx
+++ KaraokeApp.jsx
@@ -1,13 +1,22 @@
-// KaraokeApp.jsx
 import React from "react";
+import YouTube from "react-youtube";
 
 function KaraokeApp() {
+  const opts = {
+    height: "390",
+    width: "640",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
   return (
-    <div>
+    <div className="app">
       <h1>Karaoke Web App</h1>
       <p>Ứng dụng karaoke chạy trên YouTube 🎤</p>
+      <YouTube videoId="dQw4w9WgXcQ" opts={opts} />
     </div>
   );
 }
 
-export default KaraokeApp;  // 🔥 Bắt buộc có dòng này
+export default KaraokeApp;
