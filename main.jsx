6c1a96f8397805391dab5c1abfd72dafebdad910
tranongtan-max <tranongtan@gmail.com>
2025-09-08 15:28:38 +0700

    Delete main.jsx

--- main.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import KaraokeApp from "./KaraokeApp.jsx";
-
-createRoot(document.getElementById("root")).render(<KaraokeApp />);
